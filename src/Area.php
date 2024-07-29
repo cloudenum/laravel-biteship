071eb4686c4d8b98580111374ae48fc4ac8701c6
cloudenum <23111954+cloudenum@users.noreply.github.com>
2024-07-29 19:20:24 +0700

    Add support for Map Area API double search

--- src/Area.php
+++ src/Area.php
@@ -38,17 +38,41 @@ class Area extends BiteshipObject
     ];
 
     /**
-     * Search for areas by single search input
+     * Search for areas
+     *
+     * @param string  $input  The search input
+     * @param string  $countries  The country code
+     * @param bool  $double  True for double search, single search otherwise
      *
      * @return \Illuminate\Support\Collection<Area>
      *
      * @see https://biteship.com/id/docs/api/maps/retrieve_area_single
      */
-    public static function search(string $input, string $countries = 'ID'): \Illuminate\Support\Collection
+    public static function search(string $input, string $countries = 'ID', bool $double = false): \Illuminate\Support\Collection
     {
-        $params = compact('input', 'countries');
+        $params = [
+            'input' => $input,
+            'countries' => $countries,
+            'type' => $double ? 'double' : 'single',
+        ];
 
-        $response = Biteship::api()->get(self::$apiUri, $params);
+        $response     = Biteship::api()->get(self::$apiUri, $params);
+        $responseJson = $response->json();
+
+        return collect($responseJson['areas'])->map(function (array $attributes) {
+            return new static($attributes);
+        });
+    }
+
+    /**
+     * Search for areas by double search input.
+     *
+     * @param string $id The area id from previous search result
+     * @return \Illuminate\Support\Collection<Area>
+     */
+    public static function doubleSearchSecondRequest(string $id): \Illuminate\Support\Collection
+    {
+        $response     = Biteship::api()->get(self::$apiUri . '/' . $id);
         $responseJson = $response->json();
 
         return collect($responseJson['areas'])->map(function (array $attributes) {
